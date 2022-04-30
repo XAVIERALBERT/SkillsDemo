47a61271b062ce4f9318f4f43ac1759fba5d1ca9
XAVIERALBERT <xav.al.cruz@gmail.com>
2022-04-30 14:59:51 -0700

    added lib and tester

---
+++ skillsdemotester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillsdemotester {
+    @Test
+    public void greaterThanFiveTest() {
+        assertEquals(true, skillsdemo.greaterThanFive(4));
+    }
+}
